99061d5310bb3e39e722b04f858801bc2b678285
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-11-01 16:38:50 +0300

    last-update

--- MusicAlbum/src/components/Header.js
+++ MusicAlbum/src/components/Header.js
@@ -22,32 +22,27 @@ const Header = ({ headerText }) => {
 
 const styles = StyleSheet.create({
     textStyle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 15,
-    color: 'wheat',
-    
-    
-    
-
-    
-  },
-  viewStyle: {
-    backgroundColor: '#f8f8f8',
-    height: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 15,
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    borderWidth: 5,
-    borderRadius: 15,
-    width: 350,
-    height: 50,
-    marginTop: 30,
-    alignSelf: 'center',
-    borderColor: 'wheat',
-  }
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 15,
+        color: 'wheat',   
+    },
+    viewStyle: {
+        backgroundColor: '#f8f8f8',
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 15,
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        borderWidth: 5,
+        borderRadius: 15,
+        width: 350,
+        height: 50,
+        marginTop: 30,
+        alignSelf: 'center',
+        borderColor: 'wheat',
+    }
 
 });
 
